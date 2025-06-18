a84b96b4971c6ef2b7ece552020c82787728e48f
hbakhtiar <hbakhtiar@wisc.edu>
2025-06-18 15:47:28 -0500

    Update Postprocess_nuclei.py

--- Postprocess_nuclei.py
+++ Postprocess_nuclei.py
@@ -1,1 +1,21 @@
 
+
+
+def process_image(args,lock):
+
+  predicted_image_name,results_folder_path,cleaned_segmentation_folder_path,x_spacing,y_spacing,z_spacing,original_study,json_output_path = args
+  
+
+
+
+
+def worker(input_queue,lock):
+  while True:
+    args = input_queue.get()
+    if args is None:
+      break
+      
+    process_image(args,lock)
+
+
+
