fbc0d6f78769c1ff1a92e37f286a92e3d1acab5b
hbakhtiar <hbakhtiar@wisc.edu>
2025-06-18 11:25:50 -0500

    Update Organoid_segmentation.py

--- Organoid_segmentation.py
+++ Organoid_segmentation.py
@@ -2,14 +2,19 @@
 import os
 import SimpleITK as sitk
 import numpy as np
-from skimage.filters import threshold_triangle
+from skimage.filters import threshold_triangle, threshold_otsu
+from skimage.measure import regionprops
+from skimage.morphology import remove_small_objects
+
 
 
 #function for performing triangle thresholding
 #uses threshold_triangle to get threshold, then converts image to binary
 
-def triangle_threshold(grayscale_image):
-  
+def triangle_threshold_4_binary(image):
+  threshold = threshold_triangle(image)
+  binary_image = (image > threshold).astype(np.uint8)
+  return binary_image
 
 #Images should be converted to .nii.gz format, with only nuclear stain channel
 #Image should have format (z,y,x)
@@ -17,6 +22,7 @@ def triangle_threshold(grayscale_image):
 images_folder = ''
 saveImage = ''
 nuclear_stain = ''
+minimum_size = ''
 
 images_2_segment_list = os.listdir(images_folder)
 images_2_segment_list = [image_name for image_name in images_2_segment_list if image_name.endswith('.nii.gz')]
@@ -31,38 +37,29 @@ for image_name in image_2_segment_list:
 
     grayscale_image = sitk.ReadImage(image_path) #obtain image
     grayscale_image = sitk.GetArrayFromImage(grayscale_image) #convert to numpy array
-
-    max_threshold = 0 # initialize threshold
+    thresheld_image = triangle_threshold_4_binary(grayscale_image) #threshold image
   
     final_image = np.zeros_like(grayscale_image) #final_image so can populate by z layer
     num_z_layers = grayscale_image.shape[0]
 
-    thresheld_image = loadFunctions.triangle_threshold(grayscale_image)
-
-    num_z_layers = thresheld_image.shape[0]
-    final_image = np.zeros_like(thresheld_image)
-    filtered_image = np.zeros_like(thresheld_image)
+    #first obtain a maximum otsu threshold value across all z layers
+  
     max_threshold = 0
 
-
     for z in range(num_z_layers):
-
 
         grayscale_z = grayscale_image[z,:,:]
         threshold_value = filters.threshold_otsu(grayscale_z)
         if threshold_value > max_threshold:
             max_threshold=threshold_value
 
-
-
-
+  #once a max threshold is found, apply across z layers
+  
     for z in range(num_z_layers):
 
         grayscale_z = grayscale_image[z,:,:]
-        thresheld_z = (grayscale_z > max_threshold).astype(np.uint8)
-
+        thresheld_z = (grayscale_z > max_threshold).astype(np.uint8) 
         labeled_array = skimage.measure.label(thresheld_z, connectivity=2)
-
 
         # Get properties of the connected components
         regions = regionprops(labeled_array)
@@ -73,13 +70,14 @@ for image_name in image_2_segment_list:
         for region in regions:
             volume_list.append(region.area)
 
-        # Calculate the average volume of the regions
-        volume_array = np.asarray(volume_list)
-        average_volume = np.mean(volume_array)
-        standard_dev = np.std(volume_array)
+        if min_size is None:
+          
+          # Calculate the average volume of the regions, use average volume by default
+          volume_array = np.asarray(volume_list)
+          min_size = np.mean(volume_array)
 
         # Remove regions smaller than the average volume
-        labeled_array_filtered = remove_small_objects(labeled_array, min_size=average_volume)
+        labeled_array_filtered = remove_small_objects(labeled_array, min_size=min_size) #
         # labeled_array_filtered = labeled_array_filtered
 
         # Convert the labeled array back to a binary image
