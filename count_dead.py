6a4d6b39cd5e72a12cc840452e0aa98af1a4a4cb
hbakhtiar <hbakhtiar@wisc.edu>
2025-06-19 14:12:17 -0500

    Update count_dead.py

--- count_dead.py
+++ count_dead.py
@@ -1,1 +1,60 @@
 
+
+import os
+import SimpleITK as sitk
+
+
+segmented_organoids_folder = ''
+original_image_folder = ''
+death_marker_channel=''
+json_output_path = ''
+
+segmented_organoids_list = [file for file in segmented_organoids_folder if file.endswith('.nii.gz')]
+
+for segmented_image_name in segmented_organoids_list:
+
+      root_name = segmented_image_name.split('.')[0]
+      all_channels_name = root_name + '_allChannels.nii.gz'
+
+      segmented_organoid_path = os.path.join(segmented_organoids_folder,image_name)
+      organoid_channels_path = os.path.join(original_image_folder,all_channels_name)
+
+      segmented_organoid_cropped = sitk.ReadImage(segmented_organoid_path)
+      segmented_organoid_cropped = sitk.GetArrayFromImage(segmented_organoid_cropped)
+
+      original_image_cropped = sitk.ReadImage(organoid_channels_path)
+      original_image_cropped = sitk.GetArrayFromImage(original_image_cropped)
+      
+      
+      death_marker_channel  = original_image_cropped[:,:,:,death_marker_channel]
+
+      flattened_image = death_marker_channel.flatten()
+      threshold = threshold_otsu(flattened_image)
+      death_marker_channel_thresheld = death_marker_channel > threshold
+      component_labels = np.unique(segmented_organoid_cropped)
+      component_labels = component_labels[component_labels > 0]  # Remove background label (0)
+
+      components_with_overlap = []
+
+
+      for component_id in component_labels:
+        component_mask = (segmented_organoid_cropped == component_id)
+
+        for z in range(segmented_organoid_cropped.shape[0]):
+          component_slice = component_mask[z,:,:]
+          death_channel_slice = death_marker_channel_threshold[z,:,:]
+
+          component_size = np.sum(component_slice > 0)
+          if component_size ==0:
+            continue
+
+        overlap = np.sum(component_slice & death_channel_slice)
+
+        if overlap / component_size >= 0.75: # 75% of component should have overlap 
+
+          components_with_overlap.append(component_id)
+          break
+
+      
+
+      
