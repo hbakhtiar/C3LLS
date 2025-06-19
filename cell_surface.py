d81f4a2022646eb96e79de7753722922a25f4797
hbakhtiar <hbakhtiar@wisc.edu>
2025-06-19 14:00:22 -0500

    Update cell_surface.py

--- cell_surface.py
+++ cell_surface.py
@@ -17,23 +17,6 @@ def max_distance_centroid_to_background(component_mask):
       return np.max(dist)
 
 
-# Custom Process class to override the daemon flag
-#I use this, but you should make sure that your server can handle
-#if not, just use a regular pool with multiprocessing
-class NonDaemonProcess(multiprocessing.Process):
-    def _get_daemon(self):
-        return False
-    def _set_daemon(self, value):
-        pass
-    daemon = property(_get_daemon, _set_daemon)
-
-# Custom Pool class that uses NonDaemonProcess
-class NonDaemonPool(Pool):
-    def Process(self, *args, **kwargs):
-        proc = super().Process(*args, **kwargs)
-        proc.__class__ = NonDaemonProcess
-        return proc
-
 
 
 #names got too long and confusing so renamed to binary1 and binary2
@@ -112,96 +95,47 @@ def sum_pixels_and_volume_by_label_fast(labeled_image,original_image):
     })
 
 
-def secondary_worker(args):
 
-    label,bbox,cell_surface_shm_name,cell_surface_dtype,shape,nuclear_segmentation_path,sampleID,json_output_path,lock,name = args
-    unique_organoid_id = cantor_pair(sampleID,label)
-    unique_organoid_name = f'ORGANOID_{unique_organoid_id:03}.nii.gz'
+segmented_organoids_folder = ''
+original_image_folder = ''
+cell_surface_channel=''
+json_output_path = ''
 
-    segmented_organoid_path = os.path.join(nuclear_segmentation_path,unique_organoid_name)
+segmented_organoids_list = [file for file in segmented_organoids_folder if file.endswith('.nii.gz')]
 
-    if not os.path.exists(segmented_organoid_path):
-        return
+for segmented_image_name in segmented_organoids_list:
 
-    segmented_organoid_cropped = sitk.ReadImage(segmented_organoid_path)
-    segmented_organoid_cropped = sitk.GetArrayFromImage(segmented_organoid_cropped)
+      root_name = segmented_image_name.split('.')[0]
+      all_channels_name = root_name + '_allChannels.nii.gz'
 
-    #get cell surface shared object
+      segmented_organoid_path = os.path.join(segmented_organoids_folder,image_name)
+      organoid_channels_path = os.path.join(original_image_folder,all_channels_name)
 
-    cell_surface_shm = shared_memory.SharedMemory(name = cell_surface_shm_name)
-    cell_surface_channel = np.ndarray(shape,dtype = cell_surface_dtype,buffer = cell_surface_shm.buf)
+      segmented_organoid_cropped = sitk.ReadImage(segmented_organoid_path)
+      segmented_organoid_cropped = sitk.GetArrayFromImage(segmented_organoid_cropped)
 
-    zmin, ymin, xmin, zmax, ymax, xmax = bbox #define bounding box 
+      original_image_cropped = sitk.ReadImage(organoid_channels_path)
+      original_image_cropped = sitk.GetArrayFromImage(original_image_cropped)
+      
+      
+      cell_surface_channel_cropped = original_image_cropped[:,:,:,cell_surface_channel]
 
-    cell_surface_channel_cropped = (cell_surface_channel[zmin:zmax, ymin:ymax, xmin:xmax])
-    flattened_image = cell_surface_channel_cropped.flatten()
-    threshold = threshold_otsu(flattened_image)
-    cell_surface_channel_cropped_thresheld = cell_surface_channel_cropped > threshold
-    cell_surface_labeled_image = assign_foreground_by_boundary_expansion(segmented_organoid_cropped,cell_surface_channel_cropped_thresheld)
-    cell_surface_scores_df = sum_pixels_and_volume_by_label(cell_surface_labeled_image,cell_surface_channel_cropped)
-    results = cell_surface_scores_df.to_dict(orient='list')
+      flattened_image = cell_surface_channel_cropped.flatten()
+      threshold = threshold_otsu(flattened_image)
+      cell_surface_channel_cropped_thresheld = cell_surface_channel_cropped > threshold
+      cell_surface_labeled_image = assign_foreground_by_boundary_expansion(segmented_organoid_cropped,cell_surface_channel_cropped_thresheld)
+      cell_surface_scores_df = sum_pixels_and_volume_by_label(cell_surface_labeled_image,cell_surface_channel_cropped)
+      results = cell_surface_scores_df.to_dict(orient='list')
 
-    with lock:
-        with open(json_output_path,"a") as f:
+
+      with open(json_output_path,"a") as f:
             json.dump(results,f)
             f.write('\n')
             f.flush()
-
-    cell_surface_shm.close()
-
-    del segmented_organoid_cropped,cell_surface_labeled_image,cell_surface_channel_cropped_thresheld,cell_surface_channel_cropped
-
-    return
-
-
-def primary_worker(args):
-
-      sampleID,segmented_organoids_folder,nuclear_segmentation_path,json_output_path,lock,nii_gz_path,name,cell_surface_channel = args
-
-      segmented_organoid_name = f'ORGANOID_{sampleID:03}.nii.gz'
-      segmented_organoid_filepath = os.path.join(segmented_organoids_folder,segmented_organoid_name)
-
-      segmented_organoids = sitk.ReadImage(segmented_organoid_file_path)
-      segmented_organoids = sitk.GetArrayFromImage(segmented_organoids)
-
-      segmented_organoids = skimage.measure.label(segmented_organoids,connectivity=2)
-      properties = skimage.measure.regionprops(segmented_organoids)
-
-
-      cell_surface_name = f'ORGANOIDS_{sampleID:03}_{cell_surface_channel:04}.nii.gz'
-      cell_surface_path = os.path.join(channels_path,cell_surface_name)
-      cell_surface_channel = sitk.ReadImage(cell_surface_path)
-      cell_surface_channel = sitk.GetArrayFromImage(cell_surface_channel)
-
-      #create shared memory object for the image of the channel
-      shm_surface = shared_memory.SharedMemory(create = True,size = cell_surface_channel.nbytes)
-      surface_shm_array = np.ndarray(cell_surface_channel.shape,dtype=cell_surface_channel.dtype,buffer=shm_surface.buf)
-
-      args_list = [(prop.label,prop.bbox,shm_surface.name,cell_surface_channel.dtype,cell_surface_channel.shape,nuclear_segmentation_path,sampleID,json_output_path,lock,name) for prop in properties]
-
-      with NonDaemonPool(processes=8) as pool:
-            pool.map(secondary_worker,args_list)
-
-      shm_surface.close()
-      shm_surface.unlink()
-
-      del cell_surface_channel,segmented_organoids,segmented_organoids_objects_removed
-      gc.collect()
-
-      return
-
-
-segmented_organoids_folder= ''
-channels_path = ''
-
       
       
-##NEED TO SETUP PIPELINE
 
-    
 
-args_list = [(sampleID,segmented_organoids_folder,nuclear_segmentation_path,json_output_path,lock,channels_path,name,cell_surface_channel) for sampleID in segmented_organoids_id_list]
 
-with NonDaemonPool(processes=16) as pool:
-      pool.map(primary_worker,args_list)
+
 
